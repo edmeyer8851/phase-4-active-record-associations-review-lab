ca4ce283d10cb808008bdb5a0d6997e1f172c7a2
Eddie Meyer <edmeyer8851@gmail.com>
2023-02-05 14:31:53 -0500

    commit tree 7ae3cd6c4ac6626467606e7a2a7efab64d01675c

---
+++ db/migrate/20230205192916_add_passenger_id_to_rides.rb
@@ -0,0 +1,6 @@
+class AddPassengerIdToRides < ActiveRecord::Migration[6.1]
+  def change
+    add_column :rides, :passenger_id, :integer
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
